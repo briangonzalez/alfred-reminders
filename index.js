3ee41edff5b197f6b8a371de1e34b4036ec86668
Brian Gonzalez <me@briangonzalez.org>
2017-08-09 12:14:21 -0700

    eslint

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
-'use strict';
-const alfy = require('alfy');
+'use strict'
+const alfy = require('alfy')
 const reminders = require('./lib/reminders')
 
 const lists = reminders
-	.getLists()
-	.filter(list => list.toLowerCase().includes(alfy.input.toLowerCase()))
-	.map((list, index) => {
-		const length = reminders.getRemindersInList(list).length
-		return {
-			title: `${list} List`,
-			subtitle: `Show all ${length} reminder(s) in "${list}"`,
-			arg: list
-		}
-	})
+  .getLists()
+  .filter(list => list.toLowerCase().includes(alfy.input.toLowerCase()))
+  .map((list, index) => {
+    const length = reminders.getRemindersInList(list).length
+    return {
+      title: `${list} List`,
+      subtitle: `Show all ${length} reminder(s) in "${list}"`,
+      arg: list
+    }
+  })
 
-alfy.output(lists);
+alfy.output(lists)
